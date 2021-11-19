2cd8ac1fa297a725a47dbfb2f41d7193cb971774
zhouzhengwei <zzw17370709160@163.com>
2021-11-19 18:05:46 +0800

    database

---
+++ HelloworldJsonDatabase.java
@@ -0,0 +1,96 @@
+import com.google.gson.Gson;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloworldJsonDatabase extends HttpServlet {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://47.101.196.82:3306/linux_final";
+    static final String USER = "root";
+    static final String PASS = "zzw171413";
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Connection conn = null;
+        Statement stmt = null;
+        response.setContentType("application/json;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        Gson gson = new Gson();
+        Student1 stu = new Student1();
+        try {
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = conn.createStatement();
+            String sql = "SELECT `id`, `name` FROM `linux_final`.`t_student` WHERE  `id`=1";
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                stu.setId(id);
+                stu.setName(name);
+            }
+
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        String json = gson.toJson(stu);
+        out.println(json);
+        out.flush();
+        out.close();
+    }
+
+
+}
+class Student1 {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Student1() {
+
+    }
+
+    public Student1(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
